6fede1c0535977ac0e41967ece2fbf7fb2884f1f
Mayur Singh Shekhawat <88105485+Mayursingh9111@users.noreply.github.com>
2025-03-20 21:51:41 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   server: {
     proxy: {
-      "../api": {
-        target: import.meta.env.VITE_BACKEND_API,
+      "/api": {
+        target: process.env.VITE_BACKEND_API,
         secure: false,
       },
     },
